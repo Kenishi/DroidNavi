7111461f769007a972bf62180304045e7f3db801
Kenishi <kenishi86@gmail.com>
2014-06-02 01:25:06 -0400

    Revert "Added sleep to fix server startup exceptions"
    This reverts commit a66058d8db5a536dd83148d9f82867afc8ade5d2.

--- pyqt-ui/src/pytelelog_pyqt/POpenLauncher.py
+++ pyqt-ui/src/pytelelog_pyqt/POpenLauncher.py
@@ -5,31 +5,30 @@ Created on May 18, 2014
 '''
 import sys
 import glob
-import time
 
 from PyQt4 import QtGui
-from py4j.java_gateway import JavaGateway
+from py4j.java_gateway import JavaGateway, GatewayClient
 from MainWindow import MainWindow
+from logging import StreamHandler, Logger, getLogger, DEBUG
 import subprocess
 
-class POpenLauncher:
+class Launcher:
     '''
     Launcher is used for deployment launching when the 
     JavaGateway will be in the 'lib' subfolder.
     '''
     
     def __init__(self):
-        # Look for the gateway server
-        jar_file = glob.glob('../lib/pctelelog-gateway-server*')
-        if len(jar_file) == 0:
-            jar_file = glob.glob('./lib/pctelelog-gateway-server*')
-        if len(jar_file) == 0:
-            raise IOError("pctelelog-gateway-server JAR could not be found. Please make sure the JAR is in the 'lib' folder.")
-     
-        command = ["java", "-jar", jar_file[0]]
+    	# Look for the gateway server
+    	file = glob.glob('../lib/pctelelog-gateway-server*')
+    	if len(file) == 0:
+    		file = glob.glob('./lib/pctelelog-gateway-server*')
+    	if len(file) == 0:
+    		raise IOException("pctelelog-gateway-server JAR could not be found. Please make sure the JAR is in the 'lib' folder.")
+    		
+        command = ["java", "-jar", file[0]]
         serverProc = subprocess.Popen(command)
         if(serverProc != None):
-            time.sleep(1) # Give the server time to start up
             try:
                 self.__gateway = JavaGateway(start_callback_server=True)
             except:
@@ -50,5 +49,5 @@ class POpenLauncher:
             
        
 if __name__ == '__main__':
-    POpenLauncher()
+    Launcher()
     pass
