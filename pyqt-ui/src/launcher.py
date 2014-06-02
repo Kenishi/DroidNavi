33467737e4818823e9a986cad6584b1ab1e32f38
Jeremy May <kenishi86@gmail.com>
2014-06-02 03:27:12 -0400

    Adding py2exe launcher script.

---
+++ pyqt-ui/src/launcher.py
@@ -0,0 +1,12 @@
+'''
+Created on 2014/05/31
+
+@author: vix
+'''
+from pytelelog_pyqt import *
+from pytelelog_pyqt.POpenLauncher import POpenLauncher
+from sys import path
+
+if __name__ == '__main__':
+    POpenLauncher()
+    pass
