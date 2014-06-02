c5a67ac2d3aa6a2d01bc7aa982327381a30cdbc2
Jeremy May <kenishi86@gmail.com>
2014-06-02 01:03:44 -0400

    Fixed so required files are now copied into py2exe dist correctly.

--- pyqt-ui/src/setup.py
+++ pyqt-ui/src/setup.py
@@ -6,18 +6,42 @@ Setup Script to create Compiled Windows Exe
 @author: Jeremy May
 '''
 
+import site
+import os
+import glob
 from distutils.core import setup
+
 import py2exe
 
-files = ["lib/*"]
+site_package = site.getsitepackages()[1]
+imageformats = site_package + "/PyQt4/plugins/imageformats"
 
+dat_files = []
+# Get Libs
+for file_ in os.listdir("./lib"):
+    f1 = "./lib/" + file_
+    if os.path.isfile(f1):
+        f2 = "lib", [f1]
+        dat_files.append(f2)
+
+# Get Imageformats
+for file_ in os.listdir(imageformats):
+    f1 = imageformats + "/" + file_
+    if os.path.isfile(f1):
+        f2 = "imageformats", [f1]
+        dat_files.append(f2)
+
+    
 setup(name = "DroidNavi",
       version = "0.1",
       author = "Jeremy May",
       url = "http://github.com/Kenishi",
       license = "GNU General Public License (GPL) v2",
-      packages = ['pytelelog_pyqt','pytelelog_pyqt/components'],
-      package_data = {'package' : files},
-      windows = ["pytelelog_pyqt/POpenLauncher.py"],
-      options = {"py2exe": {"skip_archive": True, "bundle_files": 1, "compressed": True, "includes": ["sip"],"dll_excludes": ["MSVCP90.dll"]}}
+      data_files = dat_files,
+      windows = [{"script": "launcher.py"}],
+      options = {"py2exe": {"bundle_files": 1,
+                            "compressed": True,
+                            "packages":["py4j", "pytelelog_pyqt"],
+                            "includes": ["sip", "PyQt4.QtCore", "PyQt4.QtGui"],
+                            "dll_excludes": ["MSVCP90.dll"]}}
       )
