5e9c89496c9769dbb8cd2a84dbd4368fbe2d9c82
Jeremy May <kenishi86@gmail.com>
2014-07-15 03:33:55 -0400

    Simplified tray display calls

--- pyqt-ui/src/pytelelog_pyqt/components/tray.py
+++ pyqt-ui/src/pytelelog_pyqt/components/tray.py
@@ -3,40 +3,53 @@ Created on Jul 14, 2014
 
 @author: Jeremy May
 '''
+from functools import partial
+
 from PyQt4.QtGui import QSystemTrayIcon
 from PyQt4.QtGui import QMenu
+from PyQt4.QtCore import pyqtSlot
 
 from AppIcon import AppIcon
 
-class Tray(QSystemTrayIcon):
+class Tray():
 
     def __init__(self, parent):
-        super(Tray, self).__init__(AppIcon())
         
+        self.tray = QSystemTrayIcon(AppIcon())
         self.parent = parent
+        
+        self.tray.setToolTip("Droid Navi")
         
         # Menu
         self.menu = QMenu()
-        self.menu.addAction("Show Connected List", self, self.maximize())
-        self.menu.addAction("Options", self.parent, self.parent.openSettings)
-        self.setContextMenu(self.menu)
+        self.menu.addAction("Show Connected List", partial(self.maximize))
+        self.menu.addAction("Options", partial(self.parent.openSettings))
+        self.menu.addAction("Exit", partial(self.parent.close))
+        self.tray.setContextMenu(self.menu)
         
+        # Connect handlers
+        self.tray.activated.connect(self.activated)
+    
+    def getTray(self):
+        return self.tray
+     
     def display(self, show):
         ''' Toggle showing the tray '''
         
         if show:
-            self.setVisible(True)
-            self.show()
+            self.tray.show()
         else:
-            self.setVisible(False)
-            self.hide()
+            self.tray.hide()
     
+    @pyqtSlot()
     def maximize(self):
         ''' Show the main window and hide tray icon '''
         
         self.display(False)
         self.parent.maximizeFromTray()
     
+    @pyqtSlot()
     def activated(self, reason):
-        if type(reason) is QSystemTrayIcon.DoubleClick:
+        if reason == QSystemTrayIcon.DoubleClick:
             self.maximize()
+    
