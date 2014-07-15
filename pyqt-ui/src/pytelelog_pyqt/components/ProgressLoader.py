9c18dea8880ae4a7c78ab156e31d3a9e1856ce46
Kenishi <kenishi86@gmail.com>
2014-07-15 03:35:51 -0400

    Added a deleteLater to try and make dialog play nice at startup. It still doesn't.

--- pyqt-ui/src/pytelelog_pyqt/components/ProgressLoader.py
+++ pyqt-ui/src/pytelelog_pyqt/components/ProgressLoader.py
@@ -7,6 +7,7 @@ Created on Jun 20, 2014
 import time
 
 from PyQt4 import QtGui
+from PyQt4 import Qt
 
 class ProgressLoader(QtGui.QDialog):
     
@@ -14,26 +15,31 @@ class ProgressLoader(QtGui.QDialog):
         super(ProgressLoader, self).__init__(parent)
         
         self.initUi()
+        self.setAttribute(Qt.Qt.WA_DeleteOnClose)
         
     def initUi(self):
-        layout = QtGui.QHBoxLayout()
+        layout = QtGui.QVBoxLayout()
+        
+        self.msg = QtGui.QLabel("Starting server...")
+        layout.addWidget(self.msg)
         
         self.progressBar = QtGui.QProgressBar()
         self.progressBar.setMinimum(0)
         self.progressBar.setMaximum(0)
-        self.progressBar.setFormat("Starting server...")
+        self.progressBar.setFormat("")
         layout.addWidget(self.progressBar)
         
         self.setLayout(layout)
         self.setModal(False)
-        
+    
     def error(self):
         self.progressBar.setMinimum(0)
         self.progressBar.setMaximum(100)
         self.progressBar.setValue(0)
-        self.progressBar.setFormat("Failed to start server.")
+        self.msg.setText("Failed to start server.")
         time.sleep(3)
         self.finish()
         
     def finish(self):
-        self.destroy()
+        self.deleteLater()
+        self.close()
