36bd07bdef5abc1b86817a4a70aa24576a60fb75
Jeremy May <kenishi86@gmail.com>
2014-08-22 15:16:44 -0400

    Removed unused reference

--- android/AndroidTeleLog/src/com/aberdyne/droidnavi/AboutFragment.java
+++ android/AndroidTeleLog/src/com/aberdyne/droidnavi/AboutFragment.java
@@ -7,8 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-
 
 public class AboutFragment extends Fragment {
 	@Override
