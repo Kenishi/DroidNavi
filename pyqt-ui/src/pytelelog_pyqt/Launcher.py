99dd0181c33c609b82ff20628deca515297b71f0
Jeremy May <kenishi86@gmail.com>
2014-08-25 00:46:50 -0400

    Fixed library locations

--- pyqt-ui/src/pytelelog_pyqt/Launcher.py
+++ pyqt-ui/src/pytelelog_pyqt/Launcher.py
@@ -4,6 +4,7 @@ Created on May 5, 2014
 @author: Jeremy May
 '''
 import sys
+import glob
 
 from PyQt4 import QtGui
 from py4j.java_gateway import JavaGateway, GatewayClient
@@ -23,7 +24,21 @@ class Launcher:
         my_logger.setLevel(DEBUG)
         my_logger.addHandler(handler)
         
-        self.__gateway = JavaGateway.launch_gateway(jarpath="./lib/py4j-0.8.1.jar", classpath="./lib/pctelelog*.jar", die_on_exit=True)
+        if glob.glob("./lib/py4j-0.8.1.jar"):
+            jarpath = glob.glob("./lib/py4j-0.8.1.jar")[0]
+        elif glob.glob("./py4j-0.8.1.jar"):
+            jarpath = glob.glob("./py4j-0.8.1")
+        else:
+            jarpath = None
+        
+        if glob.glob("./lib/droidnavi-gateway-server*"):
+            classpath = glob.glob("./lib/droidnavi-gateway-server*")
+        elif glob.glob("./droidnavi-gateway-server*"):
+            classpath = glob.glob("./droidnavi-gateway-server*")[0]
+        else:
+            classpath = None
+            
+        self.__gateway = JavaGateway.launch_gateway(jarpath=jarpath, classpath=classpath, die_on_exit=True)
         print self.__gateway
         app = QtGui.QApplication(sys.argv)
         mainWindow = MainWindow(self.__gateway)
