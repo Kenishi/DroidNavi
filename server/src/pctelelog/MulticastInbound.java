cabd2c755feb2768396d60de2a2500e60bee2e78
Jeremy May <kenishi86@gmail.com>
2014-10-21 03:21:15 -0400

    Add logging

--- server/src/pctelelog/MulticastInbound.java
+++ server/src/pctelelog/MulticastInbound.java
@@ -1,17 +1,22 @@
 package pctelelog;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import pctelelog.events.AbstractEvent;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.ReferenceCountUtil;
 
 public class MulticastInbound extends ChannelInboundHandlerAdapter {
-
+	private final Logger logger = LogManager.getLogger(MulticastInbound.class);
+	
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg)
 			throws Exception {
 		AbstractEvent event = (AbstractEvent)msg;
 		EventOperator.instance().onEvent(event, false);
+		logger.debug("Event Fired: " + event.toString());
 		ReferenceCountUtil.release(msg);
 	}
 	
